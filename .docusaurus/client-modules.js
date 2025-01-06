261b02e839946f0398751055196d5f4f64d65824
uuboyscy <aegis12321@gmail.com>
2025-01-06 22:00:50 +0800

    feat: :sparkles: Initiation of website

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/workspaces/uuboyscy-engineering-logs/node_modules/infima/dist/css/default/default.css"),
-  require("/workspaces/uuboyscy-engineering-logs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/workspaces/uuboyscy-engineering-logs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/workspaces/uuboyscy-engineering-logs/src/css/custom.css"),
-];
