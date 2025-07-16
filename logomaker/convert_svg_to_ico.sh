0291366d99c9f3d038dda924a6022ffb6293fd42
uuboyscy <aegis12321@gmail.com>
2025-07-16 22:55:09 +0800

    chore: New icon

--- logomaker/convert_svg_to_ico.sh
+++ logomaker/convert_svg_to_ico.sh
@@ -9,7 +9,6 @@ inkscape logo.svg --export-type=png --export-filename=logo_tmp.png -w 256 -h 256
 # inkscape logo.svg --export-type=png --export-filename=logo_tmp.png -w 256 -h 256 --export-area-snap --export-margin=10
 
 convert logo_tmp.png -define icon:auto-resize=16,32,48,64,128,256 favicon.ico
-rm logo_tmp.png
 
 # Overwrite logo.svg and favicon.ico in static/img
 cp logo.svg ../static/img/logo.svg
