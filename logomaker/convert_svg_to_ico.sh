93717d9679f109d8b2d9c8d72b5e4c009d7b9269
uuboyscy <aegis12321@gmail.com>
2025-07-16 22:15:27 +0800

    chore: Minus modification of icon

--- logomaker/convert_svg_to_ico.sh
+++ logomaker/convert_svg_to_ico.sh
@@ -2,9 +2,9 @@
 set -x
 
 # Convert logo.svg to favicon.ico
-inkscape logo.svg --export-type=png --export-filename=logo_256.png -w 256 -h 256
-convert logo_256.png -define icon:auto-resize=16,32,48,64,128,256 favicon.ico
-rm logo_256.png
+inkscape logo.svg --export-type=png --export-filename=logo_tmp.png -w 256 -h 256
+convert logo_tmp.png -define icon:auto-resize=16,32,48,64,128,256 favicon.ico
+rm logo_tmp.png
 
 # Overwrite logo.svg and favicon.ico in static/img
 cp logo.svg ../static/img/logo.svg
