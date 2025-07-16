1583d1df88e42fba8ae374574c08407ec6e8dad3
uuboyscy <aegis12321@gmail.com>
2025-07-16 22:42:10 +0800

    chore: Update icon

--- logomaker/convert_svg_to_ico.sh
+++ logomaker/convert_svg_to_ico.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -x
 
-# Convert logo.svg to favicon.ico
-inkscape logo.svg --export-type=png --export-filename=logo_tmp.png -w 256 -h 256
+# Convert logo.svg to favicon.ico with extra safety measures
+# Use --export-area-page to ensure the entire viewBox is exported
+inkscape logo.svg --export-type=png --export-filename=logo_tmp.png -w 256 -h 256 --export-area-page --export-background-opacity=0
+
+# Alternative: you can also try adding padding with --export-area-snap
+# inkscape logo.svg --export-type=png --export-filename=logo_tmp.png -w 256 -h 256 --export-area-snap --export-margin=10
+
 convert logo_tmp.png -define icon:auto-resize=16,32,48,64,128,256 favicon.ico
 rm logo_tmp.png
 
